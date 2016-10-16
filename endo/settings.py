82e1b20f35b2ab7b72ab7884228fd7f3090e233e
nokdong <alienpro@hanmail.net>
2016-10-16 21:49:52 +0900

    login problem solved

--- endo/settings.py
+++ endo/settings.py
@@ -127,6 +127,6 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL='/accounts/login'
+LOGIN_URL='/accounts/login/'
 LOGOUT_URL='/accounts/logout/'
 LOGIN_REDIRECT_URL='/'
